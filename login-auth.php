87f778cbf80f6169a499d834898c326a00be2a50
othersyntax <102709841+othersyntax@users.noreply.github.com>
2023-03-14 12:07:09 +0800

    Update login-auth.php

--- login-auth.php
+++ login-auth.php
@@ -12,4 +12,21 @@
     }
 
 
+//erorr using validate fetch error
+
+<?php
+$v = Validator::make($data, $rules, $msg);
+        if(! $v->fails()){
+            $pps->save();
+            echo 'ok';
+        } else{
+            $errors = $v->messages()->get('*');
+            foreach($errors as $err){
+                echo $err[0] . '<br>';
+                
+            }
+        }
+
+?>
+
 ?>
